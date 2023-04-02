b296b8c1da15b4aa61a6ae71f3712e1908694dc5
AlbertoJTD <albertojtd@outlook.com>
2023-04-02 14:33:10 -0600

    Snake body added

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from turtle import Turtle, Screen
 
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('Snake Game 🐍')
+
+starting_positions = [(0,0), (-20, 0), (-40, 0)]
+
+for position in starting_positions:
+    square = Turtle(shape='square')
+    square.color('white')
+
+    square.goto(position)
+
+screen.exitonclick()
